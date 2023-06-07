7ae8b4859eaf671408ad92156138cb293f26302c
youredith <youredith3@gmail.com>
2023-06-07 18:55:30 +0900

    feat: init supabase

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -1,11 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import { createClient } from "@supabase/supabase-js";
 
-declare global {
-  var client: PrismaClient | undefined;
-}
-
-const client = global.client || new PrismaClient();
-
-if (process.env.NODE_ENV === "development") global.client = client;
-
-export default client;
+export const supabase = createClient(
+  process.env.BASE_URL,
+  process.env.SUPABASE_ANON_API_KEY
+);
