189c5f661a69fb24c82cb4daeaf8fd780b14eade
youredith <youredith3@gmail.com>
2023-08-11 14:47:52 +0900

    refactor: generalize SimilarArtists component as a Tile

---
+++ src/components/Tile/HorizontalTiles.tsx
@@ -0,0 +1,44 @@
+import { ArtistTiles } from "@/styles/Artist/Artist";
+import { SectionContainer, SectionTitle } from "@/styles/Section";
+import { useRouter } from "next/navigation";
+import Tile from "./Tile";
+
+interface HorizontalTilesProps {
+  sectionTitle?: string;
+  arr: any[];
+  isCircle?: boolean;
+}
+interface itemProps {
+  name: string;
+}
+
+function HorizontalTiles({
+  sectionTitle,
+  arr,
+  isCircle,
+}: HorizontalTilesProps) {
+  const router = useRouter();
+  if (arr.length === 0) return <></>;
+
+  const handleTileClick = (name: string) => {
+    router.push(`/${arr}/${name}`);
+    return;
+  };
+
+  return (
+    <SectionContainer>
+      <SectionTitle>{sectionTitle}</SectionTitle>
+      <ArtistTiles>
+        {arr.slice(0, arr.length - 1).map((item: itemProps, index) => (
+          <Tile
+            key={index}
+            item={item.name}
+            handleTileClick={() => handleTileClick(item.name)}
+            isCircle={isCircle}
+          />
+        ))}
+      </ArtistTiles>
+    </SectionContainer>
+  );
+}
+export default HorizontalTiles;
---
+++ src/components/Tile/Tile.tsx
@@ -0,0 +1,41 @@
+import { useArtistImage } from "@/hooks/useArtistImage";
+import Image from "next/image";
+import TileSkeleton from "./TileSkeleton";
+
+interface TileProps {
+  item: string;
+  isCircle?: boolean;
+  handleTileClick: (artist: string) => void;
+}
+
+function Tile({ item, isCircle, handleTileClick }: TileProps) {
+  const imgUrl = useArtistImage(item);
+
+  if (!imgUrl) {
+    return <TileSkeleton isCircle={isCircle} />;
+  }
+
+  return (
+    <div
+      className="flex flex-col justify-start align-middle  items-center hover:cursor-pointer "
+      onClick={() => handleTileClick(item)}
+    >
+      <div
+        className={`${
+          isCircle ? "rounded-full h-28 w-28" : "rounded-md h-40 w-40"
+        } overflow-hidden bg-cover aspect-square`}
+      >
+        <Image
+          src={imgUrl}
+          width={isCircle ? 120 : 170}
+          height={isCircle ? 120 : 170}
+          alt={`${item} image`}
+          placeholder="blur"
+          blurDataURL="/images/default_user_avatar.jpeg"
+        />
+      </div>
+      <span className="font-semibold text-md mt-2 text-center">{item}</span>
+    </div>
+  );
+}
+export default Tile;
