fc50f0f5380503e96765dbce2b481369464f4fc4
youredith <110220308+jieuncodes@users.noreply.github.com>
2023-08-27 19:15:35 +0900

    chore: set storage option to persist session

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -6,13 +6,13 @@ export const dynamic = "force-dynamic";
 
 export async function GET() {
   const todayTop50 = await fetchCherryMusicTracks({ query: "top" });
-
   const { data, error } = await supabase
     .from("todayTop")
     .upsert(todayTop50)
     .select();
 
   if (error) throw error;
+  console.log("", `** ${todayTop50.length} has uploaded on the Supabase`);
 
   return NextResponse.json({ ok: true });
 }
--- src/lib/server/client.ts
+++ src/lib/server/client.ts
@@ -3,5 +3,6 @@ import { Database } from "@/lib/server/database.types";
 
 export const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  { auth: { persistSession: false } }
 );
