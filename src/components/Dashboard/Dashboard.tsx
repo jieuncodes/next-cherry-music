ac69b635c2729efd41df836dbccc6efa1c092568
youredith <110220308+jieuncodes@users.noreply.github.com>
2023-09-12 19:43:05 +0900

    feat: Add dashboard login instruction

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-import { useUser } from "@supabase/auth-helpers-react";
+import { SupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import FavoriteTracks from "./FavoriteTracks";
 import FavoriteArtists from "./FavoriteArtists";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 function Dashboard() {
   const user = useUser();
+  const supabaseClient = useSupabaseClient();
 
   return (
     <>
@@ -15,9 +19,26 @@ function Dashboard() {
           <FavoriteArtists />
         </>
       ) : (
-        <>
-          <div>Login to see Dashboard page</div>
-        </>
+        <div className="p-10 pr-20">
+          <h1>Please Login to see Dashboard page !!</h1>
+          <Auth
+            supabaseClient={supabaseClient}
+            providers={["github", "google", "kakao"]}
+            magicLink={true}
+            appearance={{
+              theme: ThemeSupa,
+              variables: {
+                default: {
+                  colors: {
+                    brand: "#ff5173",
+                    brandAccent: "#ff5174ca",
+                  },
+                },
+              },
+            }}
+            theme="light"
+          />
+        </div>
       )}
     </>
   );
